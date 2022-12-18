8ac8587ed0e2d035adb881e327d65c2694bdd677
Arthur Passos <arthur.ti@outlook.com>
2022-12-18 17:50:52 -0300

    a

--- tests/ci/sign_release.py
+++ tests/ci/sign_release.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 import sys
 import logging
+from env_helper import GPG_KEY
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    print(f"hello ${gpg_key}")
+    print(f"hello ${GPG_KEY}")
 
 if __name__ == "__main__":
     main()
