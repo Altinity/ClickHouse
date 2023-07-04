ae95c8ccd89a176c647351f25f86b40221c74f2b
Vasily Nemkov <V.Nemkov@gmail.com>
2023-07-04 15:57:56 +0200

    A dummy script instead of clickhouse-diagnostics

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -63,7 +63,7 @@ if [ -n "$MAKE_DEB" ]; then
   # NOTE: this is for backward compatibility with previous releases,
   # that does not diagnostics tool (only script).
   if [ -d /build/programs/diagnostics ]; then
-    if [ -z "$SANITIZER" ]; then
+    # if [ -z "$SANITIZER" ]; then
       # We need to check if clickhouse-diagnostics is fine and build it
     #   (
     #     cd /build/programs/diagnostics
@@ -71,11 +71,11 @@ if [ -n "$MAKE_DEB" ]; then
     #     GOARCH="${DEB_ARCH}" CGO_ENABLED=0 make VERSION="$VERSION_STRING" build
     #     mv clickhouse-diagnostics ..
     #   )
-        echo "NOT building clickhouse-diagnostics"
-    else
+    #    echo "NOT building clickhouse-diagnostics"
+    # else
       echo -e "#!/bin/sh\necho 'Not implemented for this type of package'" > /build/programs/clickhouse-diagnostics
       chmod +x /build/programs/clickhouse-diagnostics
-    fi
+    # fi
   fi
 fi
 
