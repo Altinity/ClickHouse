16f3738ff6bea4109002b62767dd043ca14b3ce3
MyroTk <myroslav6@gmail.com>
2023-10-24 15:24:16 -0700

    increase fast test timeout

--- tests/ci/fast_test_check.py
+++ tests/ci/fast_test_check.py
@@ -147,7 +147,7 @@ def main():
         os.makedirs(logs_path)
 
     run_log_path = os.path.join(logs_path, "run.log")
-    with TeePopen(run_cmd, run_log_path, timeout=40 * 60) as process:
+    with TeePopen(run_cmd, run_log_path, timeout=60 * 60) as process:
         retcode = process.wait()
         if retcode == 0:
             logging.info("Run successfully")
