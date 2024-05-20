06067ca795e2ece32d0b273ad93a18f6117913bb
Vasily Nemkov <V.Nemkov@gmail.com>
2024-05-20 13:58:14 +0000

    Fixed changed_images location for integration tests

--- tests/ci/docker_pull_helper.py
+++ tests/ci/docker_pull_helper.py
@@ -33,7 +33,7 @@ def get_images_with_versions(
                 break
 
     if not images_path:
-        logging.info("Images file not found")
+        logging.info(f"Images file not found in {reports_path}")
     else:
         logging.info("Images file path %s", images_path)
 
