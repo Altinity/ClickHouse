6aafc4ad96e5f61248103dfe2a4c25477bba3995
divanik <dan.ivanik@clickhouse.com>
2024-08-12 12:52:11 +0000

    Remove table registration

--- src/TableFunctions/TableFunctionObjectStorage.h
+++ src/TableFunctions/TableFunctionObjectStorage.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <Disks/ObjectStorages/IObjectStorage_fwd.h>
-#include <Formats/FormatFactory.h>
-#include <Storages/ObjectStorage/StorageObjectStorage.h>
-#include <Storages/VirtualColumnUtils.h>
-#include <TableFunctions/ITableFunction.h>
 #include "config.h"
+#include <TableFunctions/ITableFunction.h>
+#include <Formats/FormatFactory.h>
+#include <Disks/ObjectStorages/IObjectStorage_fwd.h>
+#include <Storages/VirtualColumnUtils.h>
+#include <Storages/ObjectStorage/StorageObjectStorage.h>
 
 namespace DB
 {
@@ -14,7 +14,6 @@ class Context;
 class StorageS3Configuration;
 class StorageAzureConfiguration;
 class StorageHDFSConfiguration;
-class StorageLocalConfiguration;
 struct S3StorageSettings;
 struct AzureStorageSettings;
 struct HDFSStorageSettings;
@@ -53,17 +52,6 @@ struct HDFSDefinition
 {
     static constexpr auto name = "hdfs";
     static constexpr auto storage_type_name = "HDFS";
-};
-
-struct LocalDefinition
-{
-    static constexpr auto name = "local";
-    static constexpr auto storage_type_name = "Local";
-    static constexpr auto signature = " - path\n"
-                                      " - path, format\n"
-                                      " - path, format, structure\n"
-                                      " - path, format, structure, compression_method\n";
-    static constexpr auto max_number_of_arguments = 4;
 };
 
 struct LocalDefinition
@@ -151,6 +139,4 @@ using TableFunctionAzureBlob = TableFunctionObjectStorage<AzureDefinition, Stora
 #if USE_HDFS
 using TableFunctionHDFS = TableFunctionObjectStorage<HDFSDefinition, StorageHDFSConfiguration>;
 #endif
-
-using TableFunctionLocal = TableFunctionObjectStorage<LocalDefinition, StorageLocalConfiguration>;
 }
