8fdf04e41f548f96307e1f8bb093e70cbd259098
pufit <pufit@yandex.ru>
2024-09-16 23:53:33 +0000

    Merge pull request #65277 from arthurpassos/multi_auth_methods
    Multi auth methods

--- src/Interpreters/Session.h
+++ src/Interpreters/Session.h
@@ -44,10 +44,10 @@ public:
     Session & operator=(const Session &) = delete;
 
     /// Provides information about the authentication type of a specified user.
-    AuthenticationType getAuthenticationType(const String & user_name) const;
+    std::unordered_set<AuthenticationType> getAuthenticationTypes(const String & user_name) const;
 
     /// Same as getAuthenticationType, but adds LoginFailure event in case of error.
-    AuthenticationType getAuthenticationTypeOrLogInFailure(const String & user_name) const;
+    std::unordered_set<AuthenticationType> getAuthenticationTypesOrLogInFailure(const String & user_name) const;
 
     /// Sets the current user, checks the credentials and that the specified address is allowed to connect from.
     /// The function throws an exception if there is no such user or password is wrong.
@@ -117,6 +117,7 @@ private:
 
     mutable UserPtr user;
     std::optional<UUID> user_id;
+    AuthenticationData user_authenticated_with;
 
     std::vector<UUID> external_roles;
 
