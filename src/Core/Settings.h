cc34cd200e8fd8a5c9b939bed94b044ec9ec6f10
pufit <pufit@yandex.ru>
2024-11-19 23:06:45 +0000

    Merge pull request #70332 from zvonand/ldap-remote-roles
    Passing external user roles from query originator to other nodes

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -899,6 +899,7 @@ class IColumn;
     M(UInt64, extract_key_value_pairs_max_pairs_per_row, 1000, "Max number of pairs that can be produced by the `extractKeyValuePairs` function. Used as a safeguard against consuming too much memory.", 0) ALIAS(extract_kvp_max_pairs_per_row) \
     M(Bool, restore_replace_external_engines_to_null, false, "Replace all the external table engines to Null on restore. Useful for testing purposes", 0) \
     M(Bool, restore_replace_external_table_functions_to_null, false, "Replace all table functions to Null on restore. Useful for testing purposes", 0) \
+    M(Bool, push_external_roles_in_interserver_queries, true, "Enable pushing user roles from originator to other nodes while performing a query.", 0) \
     \
     \
     /* ###################################### */ \
