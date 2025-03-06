d5d9b06157d65b80141a28099db6aee136ca6dea
Vasily Nemkov <V.Nemkov@gmail.com>
2025-03-07 00:41:56 +0100

    Update SettingsChangesHistory.cpp

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -106,6 +106,14 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             /// Release closed. Please use 25.3
         });
         addSettingsChanges(settings_changes_history, "25.1",
+    {"24.12.2.20000",
+        // Altinity Antalya modifications
+        {
+            {"input_format_parquet_use_metadata_cache", false, false, "New setting"}, // https://github.com/Altinity/ClickHouse/pull/586
+            {"input_format_parquet_use_metadata_cache", false, true, "Turn optimization ON by default"},
+        }
+    },
+    {"24.12",
         {
             /// Release closed. Please use 25.2
             {"allow_not_comparable_types_in_order_by", true, false, "Don't allow not comparable types in order by by default"},
@@ -174,7 +182,6 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         addSettingsChanges(settings_changes_history, "24.11",
             {"object_storage_cluster", "", "", "New setting"},
             {"object_storage_max_nodes", 0, 0, "New setting"},
-            {"input_format_parquet_use_metadata_cache", 0, 1, "Optimization by default"},
         }
     },
     {"24.11",
