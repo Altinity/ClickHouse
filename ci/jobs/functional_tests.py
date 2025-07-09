7dc15492dafefcb103342217abf92ad97834fd8f
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-07-09 14:15:30 -0400

    gdb fails to attach in stateless, disable for now

--- ci/jobs/functional_tests.py
+++ ci/jobs/functional_tests.py
@@ -297,12 +297,14 @@ def main():
             # )
             res = res and CH.start()
             res = res and CH.wait_ready()
-            if res:
-                if "asan" not in info.job_name:
-                    print("Attaching gdb")
-                    res = res and CH.attach_gdb()
-                else:
-                    print("Skipping gdb attachment for asan build")
+            # NOTE (strtgbb): Disable gdb, it fails to attach, disable for now
+            # if res:
+            #     if "asan" not in info.job_name:
+            #         print("Attaching gdb")
+            #         res = res and CH.attach_gdb()
+            #     else:
+            #         print("Skipping gdb attachment for asan build")
+            # NOTE (strtgbb): Disable log tables, we don't use them
             # if res:
             #     if not Info().is_local_run:
             #         if not CH.start_log_exports(stop_watch.start_time):
