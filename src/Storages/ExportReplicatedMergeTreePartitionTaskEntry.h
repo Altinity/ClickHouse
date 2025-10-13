b884fd37dc3ef6f68086e51e6afd1bff67e6c7c0
Arthur Passos <arthur.ti@outlook.com>
2025-10-13 11:20:32 -0300

    hold parts references to prevent deletion

--- src/Storages/ExportReplicatedMergeTreePartitionTaskEntry.h
+++ src/Storages/ExportReplicatedMergeTreePartitionTaskEntry.h
@@ -1,13 +1,21 @@
 #pragma once
 
 #include <Storages/ExportReplicatedMergeTreePartitionManifest.h>
+#include <Storages/MergeTree/IMergeTreeDataPart.h>
 
 namespace DB
 {
 struct ExportReplicatedMergeTreePartitionTaskEntry
 {
+    using DataPartPtr = std::shared_ptr<const IMergeTreeDataPart>;
     ExportReplicatedMergeTreePartitionManifest manifest;
 
     std::size_t parts_to_do;
+    /// References to the parts that should be exported
+    /// This is used to prevent the parts from being deleted before finishing the export operation
+    /// It does not mean this replica will export all the parts
+    /// There is also a chance this replica does not contain a given part and it is totally ok.
+    std::vector<DataPartPtr> part_references;
 };
+
 }
