5dcfac61dee8c265e7b98ed7f73a569ad09ef959
Arthur Passos <arthur.ti@outlook.com>
2025-10-29 11:04:39 +0100

    another random fix

--- src/Storages/MergeTree/MergeTreeSequentialSource.cpp
+++ src/Storages/MergeTree/MergeTreeSequentialSource.cpp
@@ -169,7 +169,8 @@ MergeTreeSequentialSource::MergeTreeSequentialSource(
             addThrottler(read_settings.local_throttler, context->getMergesThrottler());
             break;
         case Export:
-            read_settings.local_throttler = context->getExportsThrottler();
+            addThrottler(read_settings.local_throttler, context->getExportsThrottler());
+            addThrottler(read_settings.remote_throttler, context->getExportsThrottler());
             break;
     }
 
