803a30905afa03b6fadb8ed65ac182a2fad92430
Arthur Passos <arthur.ti@outlook.com>
2025-10-29 10:58:02 +0100

    save max threads as well

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -6246,7 +6246,8 @@ void MergeTreeData::exportPartToTable(const PartitionCommand & command, ContextP
             part,
             query_context->getSettingsRef()[Setting::export_merge_tree_part_overwrite_file_if_exists],
             query_context->getSettingsRef()[Setting::output_format_parallel_formatting],
-            query_context->getSettingsRef()[Setting::output_format_parquet_parallel_encoding]);
+            query_context->getSettingsRef()[Setting::output_format_parquet_parallel_encoding],
+            query_context->getSettingsRef()[Setting::max_threads]);
 
         std::lock_guard lock(export_manifests_mutex);
 
@@ -6295,6 +6296,7 @@ void MergeTreeData::exportPartToTableImpl(
         auto context_copy = Context::createCopy(local_context);
         context_copy->setSetting("output_format_parallel_formatting", manifest.parallel_formatting);
         context_copy->setSetting("output_format_parquet_parallel_encoding", manifest.parallel_formatting_parquet);
+        context_copy->setSetting("max_threads", manifest.max_threads);
 
         sink = destination_storage->import(
             manifest.data_part->name + "_" + manifest.data_part->checksums.getTotalChecksumHex(),
