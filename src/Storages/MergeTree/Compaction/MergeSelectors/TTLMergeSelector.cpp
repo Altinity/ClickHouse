80ccd6b2300df6876e1d1b9c11e9cb061dfc1056
Mikhail Artemenko <34075916+Michicosun@users.noreply.github.com>
2025-11-21 15:29:20 +0000

    Merge pull request #90059 from ClickHouse/unblock_ttl_drop_merges_in_cold_volumes
    Unblock ttl part drops for cold volumes

--- src/Storages/MergeTree/Compaction/MergeSelectors/TTLMergeSelector.cpp
+++ src/Storages/MergeTree/Compaction/MergeSelectors/TTLMergeSelector.cpp
@@ -147,6 +147,9 @@ time_t TTLRowDeleteMergeSelector::getTTLForPart(const PartProperties & part) con
 
 bool TTLRowDeleteMergeSelector::canConsiderPart(const PartProperties & part) const
 {
+    if (part.is_in_volume_where_merges_avoid)
+        return false;
+
     if (!part.general_ttl_info.has_value())
         return false;
 
@@ -165,6 +168,9 @@ time_t TTLRecompressMergeSelector::getTTLForPart(const PartProperties & part) co
 
 bool TTLRecompressMergeSelector::canConsiderPart(const PartProperties & part) const
 {
+    if (part.is_in_volume_where_merges_avoid)
+        return false;
+
     if (!part.recompression_ttl_info.has_value())
         return false;
 
