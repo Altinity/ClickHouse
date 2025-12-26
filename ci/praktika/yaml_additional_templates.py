eeeebfae8e07a53ff24c0f77baea72f290c2c1cf
CarlosFelipeOR <carlosfelipeor@gmail.com>
2025-12-25 21:59:40 -0300

    Update commit hash for Stable 25.8.13

--- ci/praktika/yaml_additional_templates.py
+++ ci/praktika/yaml_additional_templates.py
@@ -35,7 +35,7 @@ class AltinityWorkflowTemplates:
           echo "Workflow Run Report: [View Report]($REPORT_LINK)" >> $GITHUB_STEP_SUMMARY
 """
     # Additional jobs
-    REGRESSION_HASH = "c07440a1ad14ffc5fc49ce90dff2f40c2e5f364d"
+    REGRESSION_HASH = "4832cdbd813680ad663898586129ba7667e17f49"
     ALTINITY_JOBS = {
         "GrypeScan": r"""
   GrypeScanServer:
