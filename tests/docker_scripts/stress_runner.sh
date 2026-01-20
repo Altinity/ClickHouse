fdbc342d5728a72c86c47077c7f8cc97c4e3d938
strtgbb <146047128+strtgbb@users.noreply.github.com>
2026-01-20 09:16:58 -0500

    Rebase CICD on v25.3.13.19-lts

--- tests/docker_scripts/stress_runner.sh
+++ tests/docker_scripts/stress_runner.sh
@@ -57,6 +57,10 @@ configure
 
 config_logs_export_cluster /etc/clickhouse-server/config.d/system_logs_export.yaml
 
+# NOTE(strtgbb): Trying to avoid errors that may be related to running out of resources
+export CLICKHOUSE_MAX_THREADS=8
+export CLICKHOUSE_MAX_CONCURRENT_QUERIES=4
+
 start_server
 
 setup_logs_replication
