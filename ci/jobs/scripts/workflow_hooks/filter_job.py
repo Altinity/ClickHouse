1d7c12ea52c8def358a3541c500d26b9ae65ccde
strtgbb <146047128+strtgbb@users.noreply.github.com>
2026-02-03 13:27:11 -0500

    disable new filter_job logic for now

--- ci/jobs/scripts/workflow_hooks/filter_job.py
+++ ci/jobs/scripts/workflow_hooks/filter_job.py
@@ -184,23 +184,24 @@ def should_skip_job(job_name):
         if tag in job_name:
             return True, f"Skipped, job name includes excluded tag '{tag}'"
 
+    # NOTE (strtgbb): disabled this feature for now
     # If only the functional tests script changed, run only the first batch of stateless tests
-    if changed_files and all(
-        f.startswith("ci/") and f.endswith(".py") for f in changed_files
-    ):
-        if JobNames.STATELESS in job_name:
-            match = re.search(r"(\d)/\d", job_name)
-            if match and match.group(1) != "1" or "sequential" in job_name:
-                return True, "Skipped, only job script changed - run first batch only"
+    # if changed_files and all(
+    #     f.startswith("ci/") and f.endswith(".py") for f in changed_files
+    # ):
+    #     if JobNames.STATELESS in job_name:
+    #         match = re.search(r"(\d)/\d", job_name)
+    #         if match and match.group(1) != "1" or "sequential" in job_name:
+    #             return True, "Skipped, only job script changed - run first batch only"
 
-        if JobNames.INTEGRATION in job_name:
-            match = re.search(r"(\d)/\d", job_name)
-            if (
-                match
-                and match.group(1) != "1"
-                or "sequential" in job_name
-                or "_asan" not in job_name
-            ):
-                return True, "Skipped, only job script changed - run first batch only"
+    #     if JobNames.INTEGRATION in job_name:
+    #         match = re.search(r"(\d)/\d", job_name)
+    #         if (
+    #             match
+    #             and match.group(1) != "1"
+    #             or "sequential" in job_name
+    #             or "_asan" not in job_name
+    #         ):
+    #             return True, "Skipped, only job script changed - run first batch only"
 
     return False, ""
