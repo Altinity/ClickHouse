20705cd74719761353aa45da36067fce20e5f59c
CarlosFelipeOR <carlosfelipeor@gmail.com>
2026-02-16 19:59:35 -0300

    Update regression hash - Fix Iceberg Tests

--- ci/praktika/yaml_additional_templates.py
+++ ci/praktika/yaml_additional_templates.py
@@ -35,7 +35,7 @@ class AltinityWorkflowTemplates:
           echo "Workflow Run Report: [View Report]($REPORT_LINK)" >> $GITHUB_STEP_SUMMARY
 """
     # Additional jobs
-    REGRESSION_HASH = "82f98d70609e2ca20da00a10ae2fc9060a1a6769"
+    REGRESSION_HASH = "979bb27171f92724bcd8f086989ba623f2e03fdc"
     ALTINITY_JOBS = {
         "GrypeScan": r"""
   GrypeScanServer:
