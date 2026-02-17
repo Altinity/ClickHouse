e96e6ccdd64615e931e2c28f00f08edbc35fcdcd
CarlosFelipeOR <carlosfelipeor@gmail.com>
2026-02-17 15:26:07 -0300

    Update regression hash antalya-26.1

--- ci/praktika/yaml_additional_templates.py
+++ ci/praktika/yaml_additional_templates.py
@@ -35,7 +35,7 @@ class AltinityWorkflowTemplates:
           echo "Workflow Run Report: [View Report]($REPORT_LINK)" >> $GITHUB_STEP_SUMMARY
 """
     # Additional jobs
-    REGRESSION_HASH = "c5cae9b244e0839fb307a9fb67a40fe80d93810b"
+    REGRESSION_HASH = "979bb27171f92724bcd8f086989ba623f2e03fdc"
     ALTINITY_JOBS = {
         "GrypeScan": r"""
   GrypeScanServer:
