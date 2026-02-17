920e9cc81e61116c8bf561ec9a0656ea46b68b56
Vasily Nemkov <vnemkov@altinity.com>
2026-02-17 14:50:36 +0100

    Missing stuff for compilation

--- src/Daemon/SentryWriter.h
+++ src/Daemon/SentryWriter.h
@@ -50,8 +50,8 @@ public:
 
 private:
     static std::unique_ptr<SentryWriter> instance;
-    bool initialized = false;
-    bool anonymize = false;
+    [[maybe_unused]] bool initialized = false;
+    [[maybe_unused]] bool anonymize = false;
     std::string server_data_path;
 
     explicit SentryWriter(Poco::Util::LayeredConfiguration & config);
