63e2f59f6f0a09f59cc4154b0e0e83d143d7f3a6
CarlosFelipeOR <carlosfelipeor@gmail.com>
2026-02-19 02:55:50 -0300

    Update regression hash - fix uniqtheta test

--- ci/praktika/yaml_additional_templates.py
+++ ci/praktika/yaml_additional_templates.py
@@ -36,7 +36,7 @@ class AltinityWorkflowTemplates:
           echo "Workflow Run Report: [View Report]($REPORT_LINK)" >> $GITHUB_STEP_SUMMARY
 """
     # Additional jobs
-    REGRESSION_HASH = f"979bb27171f92724bcd8f086989ba623f2e03fdc"
+    REGRESSION_HASH = f"92f4413c775fc750e826951f5d235766c562a98f"
     ALTINITY_JOBS = {
         "GrypeScan": r"""
   GrypeScanServer:
