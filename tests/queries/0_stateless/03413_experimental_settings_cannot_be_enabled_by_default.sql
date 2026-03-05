92239e86732004463ea8ef85ad05f0e75ff748c0
Andrey Zvonov <32552679+zvonand@users.noreply.github.com>
2026-03-05 16:16:52 +0300

    Merge pull request #1414 from Altinity/frontport/antalya-26.1/rendezvous_hashing
    26.1 Antalya port - improvements for cluster requests

--- tests/queries/0_stateless/03413_experimental_settings_cannot_be_enabled_by_default.sql
+++ tests/queries/0_stateless/03413_experimental_settings_cannot_be_enabled_by_default.sql
@@ -4,5 +4,10 @@
 
 -- However, some settings in the experimental tier are meant to control another experimental feature, and then they can be enabled as long as the feature itself is disabled.
 -- These are in the exceptions list inside NOT IN.
-SELECT name, value FROM system.settings WHERE tier = 'Experimental' AND type = 'Bool' AND value != '0' AND name NOT IN ('throw_on_unsupported_query_inside_transaction');
+SELECT name, value FROM system.settings WHERE tier = 'Experimental' AND type = 'Bool' AND value != '0' AND name NOT IN (
+  'throw_on_unsupported_query_inside_transaction',
+  'allow_experimental_export_merge_tree_part',
+-- turned ON for Altinity Antalya builds specifically
+  'allow_experimental_iceberg_read_optimization'
+);
 SELECT name, value FROM system.merge_tree_settings WHERE tier = 'Experimental' AND type = 'Bool' AND value != '0' AND name NOT IN ('remove_rolled_back_parts_immediately');
