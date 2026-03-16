d289296406d533148781137263462673dfe1dbc8
Andrey Zvonov <zvonov.andrey@gmail.com>
2026-03-16 11:29:03 +0100

    update tests

--- tests/queries/0_stateless/03581_iceberg_parse_partition.sql
+++ tests/queries/0_stateless/03581_iceberg_parse_partition.sql
@@ -1,3 +1,4 @@
 -- Tags: no-fasttest
+SET allow_local_data_lakes = 1;
 
 CREATE TABLE t0 (c0 Nullable(Int)) ENGINE = IcebergLocal('/file0') PARTITION BY (`c0.null` IS NULL);  -- { serverError BAD_ARGUMENTS }
