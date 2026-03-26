571b99e992c8a341d9d4336bdd5015c338632dd9
Arthur Passos <arthur.ti@outlook.com>
2026-03-26 19:15:15 -0300

    he

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -46,6 +46,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"s3_propagate_credentials_to_other_storages", false, false, "New setting"},
             {"export_merge_tree_part_filename_pattern", "", "{part_name}_{checksum}", "New setting"},
             {"use_parquet_metadata_cache", false, true, "Enables cache of parquet file metadata."},
+            {"input_format_parquet_use_metadata_cache", true, false, "Obsolete. No-op"}, // https://github.com/Altinity/ClickHouse/pull/586
         });
         addSettingsChanges(settings_changes_history, "26.1",
         {
@@ -238,7 +239,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"allow_database_iceberg", false, true, "Turned ON by default for Antalya (alias)."},
             {"allow_database_unity_catalog", false, true, "Turned ON by default for Antalya (alias)."},
             {"allow_database_glue_catalog", false, true, "Turned ON by default for Antalya (alias)."},
-
+            {"input_format_parquet_use_metadata_cache", true, true, "New setting, turned ON by default"}, // https://github.com/Altinity/ClickHouse/pull/586
             {"iceberg_timezone_for_timestamptz", "UTC", "UTC", "New setting."},
             {"object_storage_remote_initiator", false, false, "New setting."},
             {"allow_experimental_iceberg_read_optimization", true, true, "New setting."},
