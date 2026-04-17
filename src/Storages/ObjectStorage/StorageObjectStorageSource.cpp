8ebf38b7c1def17eeee5ec45ad2552bd5b9442c1
Grant Holly <grant.holly@clickhouse.com>
2026-04-17 21:22:49 +0000

    Cherry-pick of https://github.com/ClickHouse/ClickHouse/pull/102825 with unresolved conflict markers (resolution in next commit)
    ---
    Original cherry-pick message follows:
    
    Merge pull request #102825 from grantholly-clickhouse/fix_iceberg_parquet_lru_cache
    
    normalizing format check using lower case
    
    # Conflicts:
    #	src/Storages/ObjectStorage/StorageObjectStorageSource.cpp

--- src/Storages/ObjectStorage/StorageObjectStorageSource.cpp
+++ src/Storages/ObjectStorage/StorageObjectStorageSource.cpp
@@ -889,7 +889,11 @@ StorageObjectStorageSource::ReaderHolder StorageObjectStorageSource::createReade
 
         InputFormatPtr input_format;
         if (context_->getSettingsRef()[Setting::use_parquet_metadata_cache] && use_native_reader_v3
+<<<<<<< HEAD
             && (object_info->getFileFormat().value_or(configuration->getFormat()) == "Parquet")
+=======
+            && (Poco::toLower(object_info->getFileFormat().value_or(configuration->format)) == "parquet")
+>>>>>>> 393b84e8cbc (Merge pull request #102825 from grantholly-clickhouse/fix_iceberg_parquet_lru_cache)
             && !object_info->getObjectMetadata()->etag.empty())
         {
             std::optional<RelativePathWithMetadata> object_with_metadata = object_info->relative_path_with_metadata;
