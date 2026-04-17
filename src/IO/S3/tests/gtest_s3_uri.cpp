bdfed042e0732a28967aaf4eb5b9cadc32db0df5
Kanthi Subramanian <subkanthi@gmail.com>
2026-04-17 17:04:12 +0200

    Updated test to check for full urls

--- src/IO/S3/tests/gtest_s3_uri.cpp
+++ src/IO/S3/tests/gtest_s3_uri.cpp
@@ -42,20 +42,19 @@ TEST(IOTestS3URI, ResolveS3Endpoint)
 {
     using namespace DB;
 
-    auto us_east_1 = S3::resolveS3Endpoint("us-east-1");
-    ASSERT_TRUE(us_east_1.starts_with("https://"));
-    ASSERT_TRUE(us_east_1.find("us-east-1") != std::string::npos);
-
-    auto eu_west_1 = S3::resolveS3Endpoint("eu-west-1");
-    ASSERT_TRUE(eu_west_1.find("eu-west-1") != std::string::npos);
+    ASSERT_EQ(S3::resolveS3Endpoint("us-east-1"),
+              "https://s3.us-east-1.amazonaws.com");
+    ASSERT_EQ(S3::resolveS3Endpoint("eu-west-1"),
+              "https://s3.eu-west-1.amazonaws.com");
 
     auto cn_north = S3::resolveS3Endpoint("cn-north-1");
-    ASSERT_TRUE(cn_north.find("cn-north-1") != std::string::npos);
-    ASSERT_TRUE(cn_north.find(".cn") != std::string::npos)
-        << "China region should resolve to .amazonaws.com.cn, got: " << cn_north;
+    ASSERT_TRUE(cn_north.ends_with(".amazonaws.com.cn"))
+        << "China region should resolve to .amazonaws.com.cn suffix, got: " << cn_north;
+    ASSERT_TRUE(cn_north.find("cn-north-1") != std::string::npos)
+        << "Got: " << cn_north;
 
-    auto gov = S3::resolveS3Endpoint("us-gov-west-1");
-    ASSERT_TRUE(gov.find("us-gov-west-1") != std::string::npos);
+    ASSERT_EQ(S3::resolveS3Endpoint("us-gov-west-1"),
+              "https://s3.us-gov-west-1.amazonaws.com");
 }
 
 #endif
