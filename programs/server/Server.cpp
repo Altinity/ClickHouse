b7734e983bf5ae308fb178e600a041d959ee302f
Andrey Zvonov <32552679+zvonand@users.noreply.github.com>
2026-04-30 12:12:17 +0200

    Cherry-pick of https://github.com/Altinity/ClickHouse/pull/1687 with unresolved conflict markers (resolution in next commit)
    ---
    Original cherry-pick message follows:
    
    Merge pull request #1687 from Altinity/feature/antalya-26.3/pr-1414-1
    
    Antalya 26.3 port - improvements for cluster requests
    # Conflicts:
    #	src/Common/ProfileEvents.cpp
    #	src/Storages/StorageFileCluster.cpp
    #	src/Storages/StorageURLCluster.cpp
    #	tests/integration/test_s3_cluster/test.py
    #	tests/queries/0_stateless/03413_experimental_settings_cannot_be_enabled_by_default.sql

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -2651,6 +2651,8 @@ try
 
     }
 
+    global_context->startSwarmMode();
+
     {
         std::lock_guard lock(servers_lock);
         /// We should start interserver communications before (and more important shutdown after) tables.
@@ -3137,6 +3139,8 @@ try
             stop_acme_instance();
 
             is_cancelled = true;
+
+            global_context->stopSwarmMode();
 
             LOG_DEBUG(log, "Waiting for current connections to close.");
 
