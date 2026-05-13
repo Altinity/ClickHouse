529a5c6e7c7178adfb6b739a4388d447bec18bf5
strtgbb <146047128+strtgbb@users.noreply.github.com>
2026-05-13 12:46:13 -0400

    also make sure regression is ec2 only

--- ci/praktika/yaml_additional_templates.py
+++ ci/praktika/yaml_additional_templates.py
@@ -67,7 +67,7 @@ class AltinityWorkflowTemplates:
     uses: ./.github/workflows/regression.yml
     secrets: inherit
     with:
-      runner_type: altinity-regression-tester
+      runner_type: altinity-ec2-regression-tester
       commit: {REGRESSION_HASH}
       arch: release
       build_sha: ${{ github.event_name == 'pull_request' && github.event.pull_request.head.sha || github.sha }}
@@ -79,7 +79,7 @@ class AltinityWorkflowTemplates:
     uses: ./.github/workflows/regression.yml
     secrets: inherit
     with:
-      runner_type: altinity-regression-tester-aarch64
+      runner_type: altinity-ec2-regression-tester-aarch64
       commit: {REGRESSION_HASH}
       arch: aarch64
       build_sha: ${{ github.event_name == 'pull_request' && github.event.pull_request.head.sha || github.sha }}
