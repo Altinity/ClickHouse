02d52acedc65b24bf011bc1171266ea1ff8a5907
Andrey Zvonov <zvonov.andrey@gmail.com>
2026-05-19 11:51:47 +0200

    fix build

--- src/Client/CommandJWTProvider.h
+++ src/Client/CommandJWTProvider.h
@@ -1,14 +1,20 @@
 #pragma once
 
-#if USE_JWT_CPP && USE_SSL
-
-#include <Client/JWTProvider.h>
 #include <string>
 
 namespace DB
 {
 
 inline constexpr int DEFAULT_JWT_COMMAND_TIMEOUT_SECONDS = 30;
+
+}
+
+#if USE_JWT_CPP && USE_SSL
+
+#include <Client/JWTProvider.h>
+
+namespace DB
+{
 
 class CommandJWTProvider : public JWTProvider
 {
