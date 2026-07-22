ed8c4540e520ba3ca9a60b73149d25979373463c
Mikhail Koviazin <github@mkmk.aleeas.com>
2026-07-22 15:40:58 +0200

    SettingsChangesHistory: fix the version for correlated_subqueries_default_join_kind

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -39,7 +39,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// controls new feature and it's 'true' by default, use 'false' as previous_value).
         /// It's used to implement `compatibility` setting (see https://github.com/ClickHouse/ClickHouse/issues/35972)
         /// Note: please check if the key already exists to prevent duplicate entries.
-        addSettingsChanges(settings_changes_history, "25.8.29",
+        addSettingsChanges(settings_changes_history, "25.8.28",
         {
             {"correlated_subqueries_default_join_kind", "left", "right", "New setting. Default join kind for decorrelated query plan."},
         });
