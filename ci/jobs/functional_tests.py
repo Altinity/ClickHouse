0f1baf43c7c3c3673df35371568b628165efa908
strtgbb <146047128+strtgbb@users.noreply.github.com>
2026-08-27 10:32:24 -0400

    Use local Azurite for stateless filesystem-cache tests.
    Community PR removed the `azure_cache` policy because external contributors have no Azure secrets, but six tests always require that policy. Match 26.6's local Azurite configuration so these tests remain covered without credentials.
    
    https://github.com/Altinity/ClickHouse/actions/runs/33023967448
    https://github.com/Altinity/ClickHouse/pull/2270

--- ci/jobs/functional_tests.py
+++ ci/jobs/functional_tests.py
@@ -190,26 +190,6 @@ def main():
 
     runner_options += f" --jobs {nproc}"
 
-    if not info.is_local_run:
-        # TODO: find a way to work with Azure secret so it's ok for local tests as well, for now keep azure disabled
-        # os.environ["AZURE_CONNECTION_STRING"] = Shell.get_output(
-        #     f"aws ssm get-parameter --region us-east-1 --name azure_connection_string --with-decryption --output text --query Parameter.Value",
-        #     verbose=True,
-        # )
-        # NOTE(strtgbb): We pass azure credentials through the docker command, not SSM.
-        # NOTE(strtgbb): Azure credentials don't exist in community workflow
-        if info.is_community_pr:
-            print(
-                "NOTE: No azure credentials provided for community PR - disable azure storage"
-            )
-            config_installs_args += " --no-azure"
-
-            # NOTE(strtgbb): With the above, some tests are still trying to use azure, try this:
-            os.environ["USE_AZURE_STORAGE_FOR_MERGE_TREE"] = "0"
-    else:
-        print("Disable azure for a local run")
-        config_installs_args += " --no-azure"
-
     if (is_azure_storage or is_s3_storage) and is_encrypted_storage:
         config_installs_args += " --encrypted-storage"
         runner_options += f" --encrypted-storage"
